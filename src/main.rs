e1ddf1625a4c9b3ae26147a278f0d450565ea32c
4lwep <alfrevr05@gmail.com>
2025-04-04 15:16:32 +0200

    flags corregidas

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use register::Register;
+use register::CPSR;
 use crate::operations::arithmetic::*;
 use crate::operations::movement::*;
 
@@ -22,29 +23,43 @@ fn main() {
     let mut r13: Register = Register::new("r13".to_string());
     let mut r14: Register = Register::new("r14".to_string());
     let mut r15: Register = Register::new("r15".to_string());
+    let mut cpsr: CPSR = CPSR::new();
 
     mov(&mut r0, &12);
+    cpsr.set_flags(&r0);
     mov(&mut r1, &32);
+    cpsr.set_flags(&r1);
 
     add(&mut r0, &r1, &r2);
+    cpsr.set_flags(&r0);
     sub(&mut r0, &r1, &r2);
+    cpsr.set_flags(&r0);
     mul(&mut r0, &r1, &r2);
+    cpsr.set_flags(&r0);
 
     mov(&mut r3, &r1);
+    cpsr.set_flags(&r3);
 
     let ayuda_r3 = r3.clone();
     mov(&mut r3, &ayuda_r3);
+    cpsr.set_flags(&r3);
     mov(&mut r2, &5);
+    cpsr.set_flags(&r2);
     mvn(&mut r4, &r2);
+    cpsr.set_flags(&r4);
 
     let ayuda_r4 = r4.clone();
     mvn(&mut r4, &ayuda_r4);
+    cpsr.set_flags(&r4);
 
     let ayuda_r5 = r5.clone();
     add(&mut r1, &r2, &r0);
+    cpsr.set_flags(&r1);
     add(&mut r5, &ayuda_r5, &ayuda_r5);
+    cpsr.set_flags(&r5);
 
     rsb(&mut r6, &r3, &r7);
+    cpsr.set_flags(&r6);
 
     println!("{}", r0.to_string());
     println!("{}", r1.to_string());
--- src/register.rs
+++ src/register.rs
@@ -4,25 +4,16 @@ pub struct Register{
     name: String,
     size: u64,
     value: i64,
-    flags: HashMap<String, bool>
 }
 
 impl Register{
     pub fn new(name: String) -> Self{
-        Register { name, size: 64, value: 0, flags: HashMap::from([
-            ("Zero".to_string(), true), 
-            ("Negative".to_string(), false), 
-            ("Overflow".to_string(), false), 
-            ("Carry".to_string(), false)]) 
-        }
+        Register { name, size: 64, value: 0 }
     }
 
     pub fn write(&mut self, valor: i64){
         if (size_of_val(&valor) as u64) < self.size{
             self.value = valor;
-
-            self.set_flag("Zero".to_string(), self.value == 0);
-            self.set_flag("Negative".to_string(), self.value < 0);
         } else {
             println!("Error: el valor el demasiado grande");
         }
@@ -32,55 +23,20 @@ impl Register{
         self.value
     }
 
-    pub fn set_flag(&mut self, flag: String, estado: bool){
-        if self.flags.contains_key(&flag){
-            self.flags.insert(flag, estado);
-        } else {
-            println!("Nombre de flag no válido");
-        }
-    }
-
-    pub fn get_flag(&self, flag: String) -> bool{
-        if self.flags.contains_key(&flag){
-            *self.flags.get(&flag).unwrap()
-        } else {
-            false
-        }
-    }
-
     pub fn to_string(&self) -> String{
         let mut registro = String::new();
         registro += "Registro: ";
         registro += &self.name as &str;
         registro += "\t Valor: ";
         registro += &self.read().to_string() as &str;
-        registro += "\t Flags: ";
-        registro +=  &self.true_flags_to_string() as &str;
         
         registro
-    }
-
-    pub fn true_flags_to_string(&self) -> String{
-        let mut flags = String::new();
-
-        self.flags.iter().for_each(|f| {
-            if self.get_flag(f.0.to_string()) {
-                flags += f.0;
-                flags.push(' ');
-            }
-        });
-
-        if flags.len() == 0{
-            return "None".to_string()
-        }
-
-        flags
     }
 }
 
 impl Clone for Register{
     fn clone(&self) -> Self {
-        Register { name: self.name.clone(), size: self.size, value: self.value, flags: self.flags.clone() }
+        Register { name: self.name.clone(), size: self.size, value: self.value }
     }
 }
 
@@ -97,5 +53,30 @@ impl RegisterValue for Register{
 impl RegisterValue for i64{
     fn read(&self) -> i64{
         *self
+    }
+}
+
+pub struct CPSR{
+    flags: HashMap<String, bool>
+}
+
+impl CPSR{
+    pub fn new() -> Self{
+        CPSR { flags: HashMap::from([
+            ("Zero".to_string(), true), 
+            ("Negative".to_string(), false), 
+            ("Overflow".to_string(), false), 
+            ("Carry".to_string(), false)])  }
+    }
+
+    pub fn set_flags(&mut self, value: &Register){
+        let value = value.read();
+
+        self.flags.insert("Zero".to_string(), value == 0);
+        self.flags.insert("Negative".to_string(), value < 0);
+    }
+
+    pub fn check_flags(&self){
+        //TODO
     }
 }
